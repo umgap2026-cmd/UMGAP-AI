73ff61f47762ad167dde6a147f4829b296e9fea0
umgap2026-cmd <umgap2026@gmail.com>
2026-02-01 14:52:01 +0700

    Update db.py

--- db.py
+++ db.py
@@ -9,11 +9,16 @@ env_path = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 def get_conn():
+    password = os.getenv("DB_PASSWORD") or os.getenv("DB_PASS")
+    user = os.getenv("DB_USER") or os.getenv("DB_USERNAME")
+
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        user=user,
+        password=password,
         cursor_factory=RealDictCursor,
     )
+
+
