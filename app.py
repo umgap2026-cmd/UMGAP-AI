5015989fb25e283b7432fd17e087903764f14751
Rafael Andre <umgap2026@gmail.com>
2026-04-06 23:10:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 import sys
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-import os
 from flask import Flask
 from dotenv import load_dotenv
 from werkzeug.middleware.proxy_fix import ProxyFix
